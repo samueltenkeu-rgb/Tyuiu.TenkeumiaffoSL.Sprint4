6d085d31820bd6cb0e72b5e6bda7f473831d54f8
Миаффо Тенкеу Лоик Сэмюэль <tyuiu.tenkeumiaffo@outlook.com>
2025-10-30 17:48:48 +0500

    Task5

--- Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Test/DataServiceTest.cs
+++ Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Test/DataServiceTest.cs
@@ -1,11 +1,41 @@
-﻿namespace Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Test
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Lib;
+
+namespace Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Test
 {
     [TestClass]
-    public sealed class DataServiceTest
+    public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCalculate()
         {
+            DataService ds = new DataService();
+
+            int[,] array = new int[5, 5] {
+                { -5, 3, -2, 1, 4 },
+                { 2, -4, 2, -1, 5 },
+                { -2, 2, -7, 2, -8 },
+                { 1, -5, 4, -5, 4 },
+                { -1, 2, -3, 2, -4 }
+            };
+
+            int[,] res = ds.Calculate(array);
+
+            int[,] wait = new int[5, 5] {
+                { -5, 1, -2, 1, 1 },
+                { 1, -4, 1, -1, 1 },
+                { -2, 1, -7, 1, -8 },
+                { 1, -5, 1, -5, 1 },
+                { -1, 1, -3, 1, -4 }
+            };
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Assert.AreEqual(wait[i, j], res[i, j]);
+                }
+            }
         }
     }
 }
--- Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1/Program.cs
+++ Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TenkeumiaffoSL.Sprint4.Task5.V1.Lib;
+using System;
+
+Console.Title = "Спринт #4 | Выполнил: Миаффо Тенкеу Лоик Сэмюэль | ИСТНб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #1                                                              *");
+Console.WriteLine("* Выполнил: Миаффо Тенкеу Лоик Сэмюэль | ИСТНб-25-1                       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* случайными значениями в диапазоне от -9 до 8. Заменить положительные   *");
+Console.WriteLine("* элементы на 1.                                                          *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+DataService ds = new DataService();
+Random rand = new Random();
+int[,] array = new int[5, 5];
+
+// Заполняем массив случайными числами от -9 до 8
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        array[i, j] = rand.Next(-9, 9);
+    }
+}
+
+Console.WriteLine("Исходный массив:");
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int[,] result = ds.Calculate(array);
+
+Console.WriteLine("Массив после замены положительных элементов на 1:");
+for (int i = 0; i < result.GetLength(0); i++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        Console.Write(result[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadLine();
